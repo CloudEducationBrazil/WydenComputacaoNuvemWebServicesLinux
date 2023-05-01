17f91279bb003092d7ceb9740bb14d5cc0b6ca94
Heleno Cardoso <helenocardosofilho@gmail.com>
2023-05-01 20:13:41 -0300

    Cloud Computer AV1 01maio2023

---
+++ conexaoMongoDBAtlasNuvemflavio.py
@@ -0,0 +1,28 @@
+from pymongo.mongo_client import MongoClient
+
+from pymongo.server_api import ServerApi
+
+#Renato uri = "mongodb+srv://renato191108:KZ1qwEKOwE5A54hX@clusters-std-ads.6xwi1sb.mongodb.net/test" 
+#Edivaldo de Jesus uri = "mongodb+srv://edics122:Rg0am7NSBmb9hNjr@edivaldo0.vribdwl.mongodb.net/test" 
+#uri = "mongodb+srv://flavioadm:A1b2C3@provaheleno.fthmkx4.mongodb.net/test"
+#Luis Vinicius; Michael  uri = "mongodb+srv://mishiyaeru:1@cluster0.duqjftm.mongodb.net/?retryWrites=true&w=majoritY"
+
+#uri = "mongodb+srv://arseniomendes20:Ams0210021@googleclouduniruy.fbxtwzh.mongodb.net/test" # Arsenio ok
+
+#uri = "mongodb+srv://renato191108:KZ1qwEKOwE5A54hX@clusters-std-ads.6xwi1sb.mongodb.net/test"
+
+#uri = "mongodb+srv://dneym:levimercia1234@cluster0.3qwamqc.mongodb.net/test"
+server_api = ServerApi('1')
+
+try:
+
+    client = MongoClient(uri, server_api=server_api)
+
+    client.admin.command('ping')
+
+    print("Sucesso na conexão com o banco de dados")
+
+    client.close()
+except Exception as e:
+
+    print("Falha na conexão com o MongoDB:", e)
