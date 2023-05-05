b92c57071429648c4302f5ad84d7bb1f321462fe
Heleno Cardoso <helenocardosofilho@gmail.com>
2023-05-05 00:52:05 -0300

    Cloud Computers AV1 05maio2023

--- conexaoMongoDBAtlasNuvemflavio.py
+++ conexaoMongoDBAtlasNuvemflavio.py
@@ -5,13 +5,16 @@ from pymongo.server_api import ServerApi
 #Renato uri = "mongodb+srv://renato191108:KZ1qwEKOwE5A54hX@clusters-std-ads.6xwi1sb.mongodb.net/test" 
 #Edivaldo de Jesus uri = "mongodb+srv://edics122:Rg0am7NSBmb9hNjr@edivaldo0.vribdwl.mongodb.net/test" 
 #uri = "mongodb+srv://flavioadm:A1b2C3@provaheleno.fthmkx4.mongodb.net/test"
-#Luis Vinicius; Michael  uri = "mongodb+srv://mishiyaeru:1@cluster0.duqjftm.mongodb.net/?retryWrites=true&w=majoritY"
+uri = "mongodb+srv://mishiyaeru:1@cluster0.duqjftm.mongodb.net/?retryWrites=true&w=majoritY" #Vinicius Luis Nunes; Michael  
 
 #uri = "mongodb+srv://arseniomendes20:Ams0210021@googleclouduniruy.fbxtwzh.mongodb.net/test" # Arsenio ok
 
 #uri = "mongodb+srv://renato191108:KZ1qwEKOwE5A54hX@clusters-std-ads.6xwi1sb.mongodb.net/test"
 
-#uri = "mongodb+srv://dneym:levimercia1234@cluster0.3qwamqc.mongodb.net/test"
+#Mercia uri = "mongodb+srv://dneym:levimercia1234@cluster0.3qwamqc.mongodb.net/test"
+
+#uri = "mongodb+srv://guigamoraesmaciel:020219Vg@guigamoraes.4gwqhml.mongodb.net/test" # Guilherme Maciel
+
 server_api = ServerApi('1')
 
 try:
